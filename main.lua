841b070e57b7172e52c82fc82609bab40befff09
DrJamgo <drjamgo@hotmail.com>
2021-05-31 12:30:58 +0200

    FEAT: Playable Game

--- main.lua
+++ main.lua
@@ -16,7 +16,7 @@ local spaceImage = love.graphics.newImage('assets/space.jpg')
 Planet = class('Planet')
 Planet.radius = 100
 Planet.image = love.graphics.newImage('assets/world.png')
-function Planet:initialize(world,x,y)
+function Planet:initialize(world,x,y,color)
   self.body = love.physics.newBody(world, x,y, 'dynamic')
   self.shape = love.physics.newCircleShape(self.radius)
   self.fixture = love.physics.newFixture(self.body, self.shape, 1000)
@@ -24,6 +24,12 @@ function Planet:initialize(world,x,y)
   self.fixture:setFriction(10)
   self.fixture:setUserData(self)
   self.x,self.y = x,y
+  self.color = color
+
+  for i = 1, 5 do
+    local C = Junk
+    C(world, vec.add(self.x,self.y,vec.randomDirection(self.radius+C.radius)))
+  end
 end
 function Planet:update(dt)
   self.body:setPosition(self.x,self.y)
@@ -31,7 +37,7 @@ function Planet:update(dt)
 end
 function Planet:draw()
   local scale = self.radius / self.image:getWidth() * 2
-  love.graphics.setColor(0.1,0.5,1.0)
+  love.graphics.setColor(self.color)
   love.graphics.draw(self.image,self.x,self.y,0,scale,nil,self.image:getWidth()/2,self.image:getHeight()/2)
   love.graphics.setColor(1,1,1)
 end
@@ -87,17 +93,25 @@ function KeyController:draw()
 end
 
 AIController = class('AIController')
+AIController.idletime = 1
 function AIController:initialize(enemy)
   self.enemy = enemy
 end
 function AIController:update(dt)
+  self.idle = (self.idle or 0) - dt
+  if self.idle > 0 then
+    self.player:move(0)
+    return
+  end
+
   local player = self.player
   if player.junk then
     local ex,ey = self.enemy.body:getPosition()
     local vx,vy = vec.normalize(self.player.body:getLinearVelocity())
     local dx,dy = vec.normalize(vec.sub(ex,ey,self.player.body:getPosition()))
-    if vec.dot(vx,vy,dx,dy) > math.cos(10/180*math.pi) then
+    if vec.dot(vx,vy,dx,dy) > math.cos(5/180*math.pi) then
       self.player:shoot()
+      --self.idle = self.idletime
     end
   else
     local junk, dir = player.planet:getNearest(Junk, player.body:getPosition())
@@ -110,7 +124,10 @@ function AIController:update(dt)
   end
 end
 function AIController:draw()
-
+  local junk,dir = self.player.planet:getNearest(Junk,self.player.body:getPosition())
+  if junk then
+    love.graphics.line(self.player.body:getX(), self.player.body:getY(), junk.body:getPosition())
+  end
 end
 
 Player = class('Player')
@@ -118,7 +135,8 @@ Player.radius = 20
 Player.speed = 300
 Player.image = love.graphics.newImage('assets/sad-crab.png')
 function Player:initialize(world,planet,controller)
-  self.body = love.physics.newBody(gWorld,planet.body:getX(),planet.body:getY()-planet.radius-self.radius, 'dynamic')
+  local x,y = vec.add(planet.x, planet.y,vec.randomDirection(self.radius+planet.radius))
+  self.body = love.physics.newBody(gWorld,x,y, 'dynamic')
   self.shape = love.physics.newCircleShape(self.radius)
   self.fixture = love.physics.newFixture(self.body, self.shape, 1)
   self.body:setMass(0.2)
@@ -136,6 +154,7 @@ function Player:move(dir)
 end
 function Player:update(dt)
   self.controller:update(dt)
+
   local dx,dy = self.body:getX() - self.planet.body:getX(), self.body:getY() - self.planet.body:getY()
   local angle = math.atan2(dx,-dy)
   self.normal = angle-math.pi/2
@@ -173,7 +192,7 @@ end
 function Player:draw()
   local scale = self.radius / self.image:getWidth() * 2 * 1.5
   local rot = self.body:getAngle()
-  love.graphics.setColor(1,0.5,0.5)
+  love.graphics.setColor(self.planet.color)
   love.graphics.draw(self.image,self.body:getX(),self.body:getY(),rot,scale,nil,self.image:getWidth()/2,self.image:getHeight()/2)
   love.graphics.setColor(1,1,1)
   self.controller:draw()
@@ -196,16 +215,23 @@ end
 Junk = class('Junk')
 Junk.radius = 10
 Junk.color = {1,1,0.5}
+Junk.restitution = 0
+Junk.shapetype = 'rectangle'
 function Junk:initialize(world,x,y,vx,vy)
   self.body = love.physics.newBody(gWorld, x,y, 'dynamic')
-  self.shape = love.physics.newRectangleShape(self.radius*1.5,self.radius*1.5)
+  if self.shapetype == 'rectangle' then
+    self.shape = love.physics.newRectangleShape(self.radius*1.5,self.radius*1.5)
+  else
+    self.shape = love.physics.newCircleShape(self.radius)
+  end
   self.fixture = love.physics.newFixture(self.body, self.shape, 1)
-  self.body:setLinearVelocity(vx,vy)
+  self.body:setLinearVelocity(vx or 0,vy or 0)
   self.body:setMass(0.1)
   self.body:setFixedRotation(false)
   self.fixture:setUserData(self)
   self.fixture:setFriction(10)
   self.body:setAngularDamping(1)
+  self.fixture:setRestitution(self.restitution)
 end
 function Junk:shoot(vx,vy)
   self.body:setLinearVelocity(vx,vy)
@@ -221,20 +247,33 @@ function Junk:update(dt)
     end
   end
 end
-function Junk:draw2()
+function Junk:draw()
   love.graphics.setColor(self.color)
-  love.graphics.circle("fill", self.body:getX(), self.body:getY(), self.shape:getRadius())
+  if self.shape:typeOf("CircleShape") then
+      local cx, cy = self.body:getWorldPoints(self.shape:getPoint())
+      love.graphics.circle("fill", cx, cy, self.shape:getRadius())
+  elseif self.shape:typeOf("PolygonShape") then
+      love.graphics.polygon("fill", self.body:getWorldPoints(self.shape:getPoints()))
+  else
+      love.graphics.line(self.body:getWorldPoints(self.shape:getPoints()))
+  end
   love.graphics.setColor(1,1,1)
 end
+
+JunkBall = class("JunkBall", Junk)
+JunkBall.shapetype = 'circle'
+JunkBall.restitution = 0.8
 
 love.physics.setMeter(100)
 gWorld = love.physics.newWorld()
 gWorld:setGravity(0,0)
 
-local planet1 = Planet(gWorld,300,400)
-local planet2 = Planet(gWorld,900,400)
+local planet1 = Planet(gWorld,600,400,{0.1,0.7,1.0})
+local planet2 = Planet(gWorld,200,200,{1,0.5,0.5})
+local planet3 = Planet(gWorld,1000,700,{1,0.5,0.5})
 local player1 = Player(gWorld,planet1,KeyController('a','d','space'))
 local player2 = Player(gWorld,planet2,AIController(player1))
+local player3 = Player(gWorld,planet3,AIController(player1))
 
 function love.load()
 end
@@ -243,6 +282,7 @@ local G = 15000
 local atmosphere = 30
 
 function love.update(dt)
+  local dt = math.min(dt,0.1)
   for _, body in pairs(gWorld:getBodies()) do
     for _, fixture in pairs(body:getFixtures()) do
       local userdata = fixture:getUserData()
